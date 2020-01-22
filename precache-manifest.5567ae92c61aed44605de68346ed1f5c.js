dccc4e4a5fff5d1c2d2579fde6f6a1d9ef4d7fd6
Thomas Coughlin <tcoughlin@elon.edu>
2020-01-22 18:46:44 -0500

    Updates

---
+++ precache-manifest.5567ae92c61aed44605de68346ed1f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a44a1fb471da57d851b976fa06cce1",
+    "url": "/github.io/nfl-click-app/index.html"
+  },
+  {
+    "revision": "ccaa89b0254a7bf61b82",
+    "url": "/github.io/nfl-click-app/static/css/main.e9746325.chunk.css"
+  },
+  {
+    "revision": "af3e9ac0d09707b0afe6",
+    "url": "/github.io/nfl-click-app/static/js/2.7cb6da34.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/nfl-click-app/static/js/2.7cb6da34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccaa89b0254a7bf61b82",
+    "url": "/github.io/nfl-click-app/static/js/main.3a81321b.chunk.js"
+  },
+  {
+    "revision": "9dd3fb25fe11029d0a32",
+    "url": "/github.io/nfl-click-app/static/js/runtime-main.b372ca8e.js"
+  }
+]);
